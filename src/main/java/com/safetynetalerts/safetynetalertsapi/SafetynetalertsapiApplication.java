efda9b279be9fac7a4d8868d4f18920c8fb3ec08
divineion <najima.guermoudi@gmail.com>
2025-03-23 18:00:21 +0100

    Initialize a Spring Boot project with Maven and Spring Web

---
+++ src/main/java/com/safetynetalerts/safetynetalertsapi/SafetynetalertsapiApplication.java
@@ -0,0 +1,13 @@
+package com.safetynetalerts.safetynetalertsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetalertsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetalertsapiApplication.class, args);
+	}
+
+}
