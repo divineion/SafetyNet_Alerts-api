fdee8c9a1825f8408380c1f8b3875ed49b3d4984
divineion <najima.guermoudi@gmail.com>
2025-03-25 23:43:26 +0100

    refactor(#4): delegate data loading to JsonDataSetLoader
    - Replace direct file reading in JsonDataProvider with data retrieval
    from JsonDataSetLoader.
    - Autowire JsonDataSetLoader in JsonDataProvider.
    - Fetch data from the in-memory dataset provided by JsonDataSetLoader
    instead of reading from the JSON file.
    - Simplify data retrieval logic by removing file I/O and using in-memory
    data.
    
    Relates to #4

--- src/main/java/com/safetynet/safetynetalertsapi/repositories/JsonDataProvider.java
+++ src/main/java/com/safetynet/safetynetalertsapi/repositories/JsonDataProvider.java
@@ -4,33 +4,23 @@ import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.io.File;
-import java.io.IOException;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.safetynet.safetynetalertsapi.constants.DataBaseFilePaths;
+import com.safetynet.safetynetalertsapi.services.JsonDataSetLoader;
 
 @Repository
 public class JsonDataProvider implements DataProvider {
 	private static final Logger logger = LogManager.getLogger(JsonDataProvider.class);
 	
-	private static ObjectMapper objectMapper = new ObjectMapper();
-	File file = new File(DataBaseFilePaths.DATABASE_JSON_PATH);
-	private Map<String, Object> data;
+	@Autowired
+	private JsonDataSetLoader dataSetLoader;
+	
 	
 	public Map<String,Object> getAllData() {
-		data = null;
-		try {
-			data = objectMapper.readValue(file, new TypeReference<Map<String, Object>>() {});
-			logger.info("Query processed ! {} returned \n {}", file.getName(), data);
-			
-			return data;
-			} catch (IOException e) {
-				logger.error("Error reading data from Json file {}: {}", file.getName(), e.getMessage());
-			}
+		Map <String, Object> data = dataSetLoader.getDataSet();
+		logger.debug("Retrieve in-memory data");
 		return data;
 	}
 }
