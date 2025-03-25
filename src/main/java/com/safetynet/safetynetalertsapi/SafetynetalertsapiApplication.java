e97ac369b9848e50bb90dde076dfccab6daa8dbb
divineion <najima.guermoudi@gmail.com>
2025-03-25 23:33:02 +0100

    feat(#4): read JSON file
    SafetynetalertsapiApplication.java
    
    - Implement CommandLineRunner in SafetynetalertsapiApplication.
    - Autowire DataSetLoader service interface.
    - Call loadData() method from JsonData on application startup to load
    and map the JSON data.
    
    Relates to #4

--- src/main/java/com/safetynet/safetynetalertsapi/SafetynetalertsapiApplication.java
+++ src/main/java/com/safetynet/safetynetalertsapi/SafetynetalertsapiApplication.java
@@ -1,13 +1,26 @@
 package com.safetynet.safetynetalertsapi;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SafetynetalertsapiApplication {
+import com.safetynet.safetynetalertsapi.services.DataSetLoader;
 
+@SpringBootApplication
+public class SafetynetalertsapiApplication implements CommandLineRunner {
+	
+	@Autowired
+	DataSetLoader dataSetLoader;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SafetynetalertsapiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws IOException {
+		dataSetLoader.loadData();
+	}
 }
