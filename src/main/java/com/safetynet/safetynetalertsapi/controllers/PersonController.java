18642a14c0d8e05f1a656b0b21fa1e8cda3406be
divineion <najima.guermoudi@gmail.com>
2025-03-25 23:27:45 +0100

    feat(#4): json file reading
    /controllers/PersonController.java : add logs

--- src/main/java/com/safetynet/safetynetalertsapi/controllers/PersonController.java
+++ src/main/java/com/safetynet/safetynetalertsapi/controllers/PersonController.java
@@ -2,6 +2,8 @@ package com.safetynet.safetynetalertsapi.controllers;
 
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
@@ -13,15 +15,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class PersonController {
 	
+	private static Logger logger = LogManager.getLogger(PersonController.class);
+	
 	@Autowired
 	@Qualifier("finder")
 	private JsonDataFinder finder;
 	
-	//Temporary endpoint to test data fetching
-	// TODO remove before before production
+	// Temporary endpoint to test data fetching
+	// TODO remove or modify before production
+	// TODO change logger messages
 	@GetMapping("/testfetchdata")
 	public ResponseEntity<Map<String, Object>> testDataFetching() {
+		logger.debug("Request received");
 		Map<String, Object> data = finder.processQuery();
+		logger.info("Data has been fetched successfully");
 		return ResponseEntity.ok(data);
 	}
 }
