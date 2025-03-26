371793da7e8593d9ad4607369b91ab4cf5b0c75e
divineion <najima.guermoudi@gmail.com>
2025-03-26 15:51:47 +0100

    refactor(#4): load data from json file
    SafetynetalertsapiApplication.java
    
    Remove the call to the loadData() method from DataSetLoad (add
    '@PostConstruct' annotation on the method).
    
    Relates to #4

--- src/main/java/com/safetynet/safetynetalertsapi/SafetynetalertsapiApplication.java
+++ src/main/java/com/safetynet/safetynetalertsapi/SafetynetalertsapiApplication.java
@@ -1,26 +1,13 @@
 package com.safetynet.safetynetalertsapi;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.safetynet.safetynetalertsapi.services.DataSetLoader;
 
 @SpringBootApplication
-public class SafetynetalertsapiApplication implements CommandLineRunner {
-	
-	@Autowired
-	DataSetLoader dataSetLoader;
-	
+public class SafetynetalertsapiApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SafetynetalertsapiApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws IOException {
-		dataSetLoader.loadData();
 	}
 }
