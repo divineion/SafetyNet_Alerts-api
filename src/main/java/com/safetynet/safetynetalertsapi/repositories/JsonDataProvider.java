7fb8dbc4bb1d26242917084a5e2b770479e2483d
divineion <najima.guermoudi@gmail.com>
2025-04-02 21:43:27 +0200

    src/main/java/com/safetynet/safetynetalertsapi/repositories/JsonDataProvider.java
    remove unnecessary logger

--- src/main/java/com/safetynet/safetynetalertsapi/repositories/JsonDataProvider.java
+++ src/main/java/com/safetynet/safetynetalertsapi/repositories/JsonDataProvider.java
@@ -2,8 +2,6 @@ package com.safetynet.safetynetalertsapi.repositories;
 
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +12,6 @@ import com.safetynet.safetynetalertsapi.model.Person;
 
 @Repository
 public class JsonDataProvider implements DataProvider {
-	private static final Logger logger = LogManager.getLogger(JsonDataProvider.class);
 	
 	@Autowired
 	private DataSetLoader dataSetLoader;
@@ -25,21 +22,18 @@ public class JsonDataProvider implements DataProvider {
 	
 	public List<Person> findAllPersons() {
 		DataSet data = getAllData();
-		logger.info("Retrieving all persons");
 		List<Person> persons = data.getPersons();
 		return persons;
 	}
 	
 	public List<FireStation> findAllFireStations() {
 		DataSet data = getAllData();
-		logger.info("Retrieving all firestations");
 		List<FireStation> fireStations = data.getFireStations();
 		return fireStations;
 	}
 	
 	public List<MedicalRecord> findAllMedicalRecords() {
 		DataSet data = getAllData();
-		logger.info("Retrieving all medical records");
 		List<MedicalRecord> medicalRecords = data.getMedicalRecords();
 		return medicalRecords;
 	}
