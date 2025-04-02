9aeff933db135b120570eda08ccf660c485e3d07
divineion <najima.guermoudi@gmail.com>
2025-04-02 02:17:15 +0200

    feat: implement person-related endpoints
     - /personinfolastname/{lastName}: retrieves and returns a list of
    persons with a matching last name, using PersonDTO.
    
     - /communityemail/{city}: retrieves and returns a list of email
    addresses for all persons living in the given city.
    
     - /children/{address}: retrieves and returns a list of children (18
    years old or less) living at the given address, in the form of ChildDTO
    objects. Each DTO contains information about the child + a collection of
    each household member.

--- src/main/java/com/safetynet/safetynetalertsapi/controllers/PersonController.java
+++ src/main/java/com/safetynet/safetynetalertsapi/controllers/PersonController.java
@@ -1,15 +1,23 @@
 package com.safetynet.safetynetalertsapi.controllers;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.safetynet.safetynetalertsapi.model.DataSet;
-import com.safetynet.safetynetalertsapi.services.JsonDataFinder;
+import com.safetynet.safetynetalertsapi.model.Person;
+import com.safetynet.safetynetalertsapi.model.dto.ChildDTO;
+import com.safetynet.safetynetalertsapi.model.dto.PersonDTO;
+import com.safetynet.safetynetalertsapi.repositories.JsonDataProvider;
+import com.safetynet.safetynetalertsapi.services.PersonFinder;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class PersonController {
@@ -17,16 +25,66 @@ public class PersonController {
 	private static Logger logger = LogManager.getLogger(PersonController.class);
 	
 	@Autowired
-	@Qualifier("finder")
-	private JsonDataFinder finder;
+	JsonDataProvider allDataProvider;
 	
-	// Temporary endpoint to test data fetching
-	// TODO remove or modify before production
-	@GetMapping("/testfetchdata")
-	public ResponseEntity<DataSet> testDataFetching() {
-		logger.debug("GET request received for /testfetchdata endpoint");
-		DataSet data = finder.processQuery();
-		logger.info("Data has been fetched successfully");
+	@Autowired
+	private PersonFinder personFinder;
+	
+	@GetMapping("/fetchAllData")
+	public ResponseEntity<DataSet> getAllData() {
+		DataSet data = allDataProvider.getAllData();
+		
 		return ResponseEntity.ok(data);
+	}
+	
+	@GetMapping("/persons")
+	public ResponseEntity<List<PersonDTO>> getAllPersons() throws JsonProcessingException {
+			logger.debug("GET request received for /persons endpoint");
+			
+			List<PersonDTO> data = personFinder.findAll();
+			
+			logger.info("Data have been fetched successfully");
+			return ResponseEntity.ok(data);		
+	}
+	
+	@GetMapping("/personinfolastname/{lastName}")
+	public ResponseEntity<List<PersonDTO>> getPersonByLastName(@PathVariable String lastName) {
+			logger.debug("Searching for persons named "+ lastName);
+			List<PersonDTO> data = personFinder.findBy(lastName);
+			
+			return ResponseEntity.ok(data);
+	}
+	
+	@GetMapping("/communityemail/{city}")
+	public ResponseEntity<List<String>> getAllEmailByCity(@PathVariable String city) {
+		logger.debug("Searching for all email addresses in " + city);
+		List<String> data = personFinder.findAllEmail(city);
+		
+		return ResponseEntity.ok(data);
+	}
+	
+	@GetMapping("/children/{address}")
+	public ResponseEntity<List<ChildDTO>> getAllChildrenByAddress(@PathVariable String address) {
+		logger.debug("Searching for all children living at " + address);
+		try {
+			List<ChildDTO> data = personFinder.findAllChildrenByAddress(address.trim());
+			
+			return ResponseEntity.ok(data);
+		} catch(Exception e) {
+			logger.error("An error occurred while processing the request" + e);
+		}
+		return null;
+	}
+	
+	public ResponseEntity<Person> createPerson(Person person) {
+		return null;
+	}
+	
+	public ResponseEntity<Person> updatePerson(Person person) {
+		return null;
+	}
+	
+	public ResponseEntity<Person> deletePerson(Person person) {
+		return null;
 	}
 }
