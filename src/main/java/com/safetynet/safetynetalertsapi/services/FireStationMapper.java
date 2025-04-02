4697a8ea0a4548c7252e296e614825c9ef004259
divineion <najima.guermoudi@gmail.com>
2025-04-02 21:54:29 +0200

    feat(#8): implement FireStationMapper
    Add methods :
     - to map FireStation entities to FireStationDTO objects.
     - to map PersonDTO objects to CoveredPersonDTO objects.
    
    Unique object conversion or direct conversion of lists of data.

---
+++ src/main/java/com/safetynet/safetynetalertsapi/services/FireStationMapper.java
@@ -0,0 +1,37 @@
+package com.safetynet.safetynetalertsapi.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.safetynet.safetynetalertsapi.model.FireStation;
+import com.safetynet.safetynetalertsapi.model.dto.CoveredPersonDTO;
+import com.safetynet.safetynetalertsapi.model.dto.FireStationDTO;
+import com.safetynet.safetynetalertsapi.model.dto.PersonDTO;
+
+@Service
+public class FireStationMapper {
+	
+	public FireStationDTO fromFireStationToFireStationDTO(FireStation fireStation) {
+		return new FireStationDTO(fireStation.getAddress(), fireStation.getStation());
+	}
+	
+	public List<FireStationDTO> fromFireStationsToFireStationsDTO(List<FireStation> fireStations) {
+		return fireStations
+				.stream()
+				.map(this::fromFireStationToFireStationDTO)
+				.collect(Collectors.toList());
+	}
+	
+	public CoveredPersonDTO fromPersonDTOToCoveredPersonDTO(PersonDTO person) {
+		return new CoveredPersonDTO(person.getIdentity(), person.getAddress(), person.getPhone());
+	}
+	
+	public List<CoveredPersonDTO> fromCoveredPersonstoCoveredPersonsDTO(List<PersonDTO> persons) {
+		return persons
+				.stream()
+				.map(this::fromPersonDTOToCoveredPersonDTO)
+				.collect(Collectors.toList());
+	}
+}
