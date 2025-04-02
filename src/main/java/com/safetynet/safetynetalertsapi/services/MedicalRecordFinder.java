751dd8ea5314c158b0b2a3a4756f6d97042fc031
divineion <najima.guermoudi@gmail.com>
2025-04-02 02:13:41 +0200

    feat: create MedicalRecordFinder service
     - implement findAll() to retrieve all medical records.
     - add findByIdentity() to search for a medical record by the person's
    identity.
    
     - remove JsonDataFinder interface to reduce complexity

---
+++ src/main/java/com/safetynet/safetynetalertsapi/services/MedicalRecordFinder.java
@@ -0,0 +1,37 @@
+package com.safetynet.safetynetalertsapi.services;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.safetynet.safetynetalertsapi.model.Identity;
+import com.safetynet.safetynetalertsapi.model.MedicalRecord;
+import com.safetynet.safetynetalertsapi.repositories.JsonDataProvider;
+
+@Service
+public class MedicalRecordFinder {
+	
+	Logger logger = LogManager.getLogger(MedicalRecord.class);
+
+	@Autowired
+	JsonDataProvider provider;
+	
+	public List<MedicalRecord> findAll() {
+		// TODO Auto-generated method stub
+		List<MedicalRecord> medicalRecords = provider.findAllMedicalRecords();
+		
+		return medicalRecords;
+	}
+
+	public MedicalRecord findByIdentity(Identity identity) {
+		logger.debug("Recherche du MedicalRecord pour : " + identity);
+		MedicalRecord record = findAll()
+				.stream()
+				.filter(r -> r.getIdentity().toString().replace(" ", "").equalsIgnoreCase(identity.toString().replace(" ", "")))
+				.findAny().orElse(null);
+		return record;
+	}
+}
