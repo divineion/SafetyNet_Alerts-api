46bd039e936eed109a78ec794d7c282bef9d8972
divineion <najima.guermoudi@gmail.com>
2025-04-04 13:37:45 +0200

    feat: implement /phonealert endpoint
    src/main/java/com/safetynet/safetynetalertsapi/controllers/FireStationController.java
    
    Add method getPhoneCoveredByStation() that :
     - accepts a PathVariable stationNumber.
     - calls the method getCoveredPhone() from the FireStationFinder,
     - returns a ResponseEntity containing a CoveredPhoneDTO.
     - catches StationNotFoundException and returns 404 if the station
    number is not found.
    
     Refacto: move FireStationFinder and FireStationMapper to the new
    package services.finder
    
     src/main/java/com/safetynet/safetynetalertsapi/services/FireStationMapper.java
     src/main/java/com/safetynet/safetynetalertsapi/services/finders/FireStationFinder.java
    
     Add method getCoveredPhone() that :
     - verifies if the provided fire station number exists,
     - retrieves persons covered by the station,
     - extracts their unique phone,
     - returns a CoveredPhoneDTO containing the result.
    
     feat: add FireStationValidator
    
     src/main/java/com/safetynet/safetynetalertsapi/services/validators/FireStationValidator.java
    
     Create service FireStationValidator.
     Add method stationExists(int stationNumber) that:
      - retrieves all FireStation from the JsonDataProvider,
      - verifies if the given station exists,
      - returns a boolean.
    
      feat/add custom exception
    
      src/main/java/com/safetynet/safetynetalertsapi/exceptions/StationNotFoundException.java
    
      Create custom exception to handle cases where a fire station is not
    found.

--- src/main/java/com/safetynet/safetynetalertsapi/controllers/FireStationController.java
+++ src/main/java/com/safetynet/safetynetalertsapi/controllers/FireStationController.java
@@ -5,14 +5,17 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.safetynet.safetynetalertsapi.exceptions.StationNotFoundException;
+import com.safetynet.safetynetalertsapi.model.dto.CoveredPhoneDTO;
 import com.safetynet.safetynetalertsapi.model.dto.FireStationCoverageDTO;
 import com.safetynet.safetynetalertsapi.model.dto.FireStationDTO;
-import com.safetynet.safetynetalertsapi.services.FireStationFinder;
+import com.safetynet.safetynetalertsapi.services.finders.FireStationFinder;
 
 @RestController
 public class FireStationController {
@@ -34,5 +37,19 @@ public class FireStationController {
 		FireStationCoverageDTO fireStationcoverage = finder.getFireStationCoverage(stationNumber);
 		logger.info("Found " + fireStationcoverage.getAdultsCounter() + " adults and " + fireStationcoverage.getChildrenCounter() + " children");
 		return ResponseEntity.ok(fireStationcoverage);
+	}
+	
+	@GetMapping("/phonealert/{stationNumber}")
+	public ResponseEntity<CoveredPhoneDTO> getPhoneCoveredByStation(@PathVariable int stationNumber) {
+		try {
+			CoveredPhoneDTO phoneNumbers;
+			phoneNumbers = finder.getCoveredPhone(stationNumber);
+			logger.info("Phone numbers found for station " + stationNumber + ": "+ phoneNumbers.getPhones());
+			
+			return ResponseEntity.ok(phoneNumbers);
+		} catch (StationNotFoundException e) {
+			logger.error(e.getMessage());
+			return ResponseEntity.status(HttpStatusCode.valueOf(404)).body(null);
+		}
 	}
 }
---
+++ src/main/java/com/safetynet/safetynetalertsapi/exceptions/StationNotFoundException.java
@@ -0,0 +1,13 @@
+package com.safetynet.safetynetalertsapi.exceptions;
+
+public class StationNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public StationNotFoundException(String message) {
+		super(message);
+	}
+	
+}
