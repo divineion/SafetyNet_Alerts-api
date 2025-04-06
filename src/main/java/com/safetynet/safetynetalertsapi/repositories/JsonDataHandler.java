39dcf8028552ab3e0a54b866c9fb5ed277fb3b53
divineion <najima.guermoudi@gmail.com>
2025-04-06 13:22:45 +0200

    feat/add POST method :
    src/main/java/com/safetynet/safetynetalertsapi/repositories/JsonDataHandler.java
    
    Add write(Person person) method to handle adding a Person objet to the JSON file:
    
     - Instanciate and configure an ObjectMapper:
      - register the JavaTimeModule for the date/time serialization,
      - disable the serialization of
      - timestamps,
      - indent output with the default pretty printer.
    
     - Read the existing JSON file,
     - append the person object to the  appropriate section, *
     - rewrite the entire file structure.

--- src/main/java/com/safetynet/safetynetalertsapi/repositories/JsonDataHandler.java
+++ src/main/java/com/safetynet/safetynetalertsapi/repositories/JsonDataHandler.java
@@ -1,7 +1,15 @@
 package com.safetynet.safetynetalertsapi.repositories;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.safetynet.safetynetalertsapi.constants.DataBaseFilePaths;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -11,11 +19,14 @@ import com.safetynet.safetynetalertsapi.model.MedicalRecord;
 import com.safetynet.safetynetalertsapi.model.Person;
 
 @Repository
-public class JsonDataProvider implements DataProvider {
 public class JsonDataHandler implements DataHandler {
+
+	private final Logger logger = LogManager.getLogger(JsonDataHandler.class);
 	
 	@Autowired
 	private DataSetLoader dataSetLoader;
+
+	private static final File file = new File(DataBaseFilePaths.DATABASE_JSON_PATH);
 
 	public DataSet getAllData() {
 		return dataSetLoader.getDataSet();
@@ -37,5 +48,28 @@ public class JsonDataHandler implements DataHandler {
 		DataSet data = getAllData();
 		List<MedicalRecord> medicalRecords = data.getMedicalRecords();
 		return medicalRecords;
+	}
+
+	public void write(Person person) throws IOException {
+		//			//Java 8 date/time type `java.time.LocalDate` not supported by default
+		//https://www.javacodegeeks.com/jackson-java-8-date-time-localdate-support-issues.html
+		//configure ObjectMapper
+		ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule())
+				.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+				.enable(SerializationFeature.INDENT_OUTPUT);
+
+		try {
+			//représenter la structure complète du fichier
+			DataSet existingData = mapper.readValue(file, DataSet.class);
+
+			//ajouter les données
+			existingData.getPersons().add(person);
+			// réécrire tte la structure mise à jour
+			mapper.writeValue(file, existingData);  //
+
+		} catch (
+				IOException e) {
+			logger.error(e.getMessage());
+		}
 	}
 }
