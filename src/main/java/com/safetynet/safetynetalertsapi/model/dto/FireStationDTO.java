ad13170453bb354eb7efd3adb0512529d5e75f13
divineion <najima.guermoudi@gmail.com>
2025-04-06 21:26:56 +0200

    src/main/java/com/safetynet/safetynetalertsapi/model/dto/FireStationDTO.java:
    Override hashCode() and equals() method;
    Add a default constructor.

--- src/main/java/com/safetynet/safetynetalertsapi/model/dto/FireStationDTO.java
+++ src/main/java/com/safetynet/safetynetalertsapi/model/dto/FireStationDTO.java
@@ -1,9 +1,13 @@
 package com.safetynet.safetynetalertsapi.model.dto;
 
+import java.util.Objects;
+
 public class FireStationDTO {
 	private String address;
 	
 	private int station;
+
+	public FireStationDTO() {}
 	
 	public FireStationDTO(String address, int station) {
 		this.address = address;
@@ -29,5 +33,19 @@ public class FireStationDTO {
 	@Override
 	public String toString() {
 		return this.station + this.address;  
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object ) return true;
+		if (object == null || this.getClass() != object.getClass()) return false;
+		FireStationDTO fireStation = (FireStationDTO) object;
+		return Objects.equals(address, ((FireStationDTO) object).getAddress()) &&
+				Objects.equals(station, ((FireStationDTO) object).getStation());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(address, station);
 	}
 }
