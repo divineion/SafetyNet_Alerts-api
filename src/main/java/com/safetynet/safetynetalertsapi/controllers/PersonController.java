a948316b0f4ec3b98ea69205417b4ca82d989f9e
divineion <najima.guermoudi@gmail.com>
2025-04-06 13:27:30 +0200

    feat/add POST method :
    src/main/java/com/safetynet/safetynetalertsapi/model/Person.java
    
    Add method createPerson(Person person), return a 201 response.
    Format logger messages

--- src/main/java/com/safetynet/safetynetalertsapi/controllers/PersonController.java
+++ src/main/java/com/safetynet/safetynetalertsapi/controllers/PersonController.java
@@ -2,43 +2,43 @@ package com.safetynet.safetynetalertsapi.controllers;
 
 import java.util.List;
 
+import com.safetynet.safetynetalertsapi.services.persisters.PersonPersister;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.safetynet.safetynetalertsapi.model.DataSet;
 import com.safetynet.safetynetalertsapi.model.Person;
 import com.safetynet.safetynetalertsapi.model.dto.ChildDTO;
 import com.safetynet.safetynetalertsapi.model.dto.PersonInfoDTO;
-import com.safetynet.safetynetalertsapi.repositories.JsonDataProvider;
+import com.safetynet.safetynetalertsapi.repositories.JsonDataHandler;
 import com.safetynet.safetynetalertsapi.services.finders.PersonFinder;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class PersonController {
 	
-	private static Logger logger = LogManager.getLogger(PersonController.class);
+	private final static Logger logger = LogManager.getLogger(PersonController.class);
 	
 	@Autowired
 	JsonDataHandler dataHandler;
 	
 	@Autowired
 	private PersonFinder personFinder;
+
+	@Autowired
+	private PersonPersister persister;
 	
 	@GetMapping("/fetchAllData")
 	public ResponseEntity<DataSet> getAllData() {
-		DataSet data = allDataProvider.getAllData();
+		DataSet data = dataHandler.getAllData();
 		
 		return ResponseEntity.ok(data);
 	}
 	
 	@GetMapping("/persons")
-	public ResponseEntity<List<Person>> getAllPersons() throws JsonProcessingException {
+	public ResponseEntity<List<Person>> getAllPersons() {
 			logger.debug("GET request received for /persons endpoint");
 			
 			List<Person> data = personFinder.findAll();
@@ -49,7 +49,7 @@ public class PersonController {
 	
 	@GetMapping("/personinfolastname/{lastName}")
 	public ResponseEntity<List<PersonInfoDTO>> getPersonByLastName(@PathVariable String lastName) {
-			logger.debug("Searching for persons named "+ lastName);
+        logger.debug("Searching for persons named {}", lastName);
 			List<PersonInfoDTO> data = personFinder.findBy(lastName);
 			
 			return ResponseEntity.ok(data);
@@ -57,7 +57,7 @@ public class PersonController {
 	
 	@GetMapping("/communityemail/{city}")
 	public ResponseEntity<List<String>> getAllEmailByCity(@PathVariable String city) {
-		logger.debug("Searching for all email addresses in " + city);
+        logger.debug("Searching for all email addresses in {}", city);
 		List<String> data = personFinder.findAllEmail(city);
 		
 		return ResponseEntity.ok(data);
@@ -65,25 +65,32 @@ public class PersonController {
 	
 	@GetMapping("/childalert/{address}")
 	public ResponseEntity<List<ChildDTO>> getAllChildrenByAddress(@PathVariable String address) {
-		logger.debug("Searching for all children living at " + address);
+        logger.debug("Searching for all children living at {}", address);
 		try {
 			List<ChildDTO> data = personFinder.findAllChildrenByAddress(address.trim());
 			return ResponseEntity.ok(data);
 		} catch(Exception e) {
-			logger.error("An error occurred while processing the request" + e);
+            logger.error("An error occurred while processing the request{}", e.getMessage());
 		}
 		return null;
 	}
+
+	/**
+	 * Create - Add a new {@link Person}
+	 * @param person An object {@link Person}
+	 * @return The person object saved
+	 */
+	@PostMapping("/person")
+	public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+            Person savedperson = persister.savePerson(person);
+			return ResponseEntity.status(201).body(savedperson);
+    }
 	
-	public ResponseEntity<Person> createPerson(Person person) {
+	public ResponseEntity<Person> updatePerson(@RequestBody Person person) {
 		return null;
 	}
 	
-	public ResponseEntity<Person> updatePerson(Person person) {
-		return null;
-	}
-	
-	public ResponseEntity<Person> deletePerson(Person person) {
+	public ResponseEntity<Person> deletePerson(@RequestBody Person person) {
 		return null;
 	}
 }
