0bbed80aaad93e3337edd4208ab0501c21142dfd
divineion <najima.guermoudi@gmail.com>
2025-04-07 11:17:47 +0200

    src.main.java.com.safetynet.safetynetalertsapi.controller.PersonController.java
    Add /person/{identity} endpoint with PUT method updatePerson().
    The method:
      - accepts a PersonDTO,
      - calls the updatePerson() method in the PersonPersister,
      - and returns ResponseEntity with a 200 status and the updated PersonDTO as the body.
    If no matching person is found, it catches and logs a ResourceNotFoundException.

--- src/main/java/com/safetynet/safetynetalertsapi/controllers/PersonController.java
+++ src/main/java/com/safetynet/safetynetalertsapi/controllers/PersonController.java
@@ -3,6 +3,7 @@ package com.safetynet.safetynetalertsapi.controllers;
 import java.util.List;
 
 import com.safetynet.safetynetalertsapi.exceptions.ResourceAlreadyExistsException;
+import com.safetynet.safetynetalertsapi.exceptions.ResourceNotFoundException;
 import com.safetynet.safetynetalertsapi.model.dto.PersonDTO;
 import com.safetynet.safetynetalertsapi.services.persisters.PersonPersister;
 import org.apache.logging.log4j.LogManager;
@@ -93,9 +94,17 @@ public class PersonController {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
     }
-	
-	public ResponseEntity<Person> updatePerson(@RequestBody Person person) {
-		return null;
+
+	@PutMapping("/person/{identity}")
+	public ResponseEntity<PersonDTO> updatePerson(@PathVariable String identity, @RequestBody PersonDTO personDto) {
+		try {
+			PersonDTO updatedPersonDto = persister.updatePerson(personDto);
+
+			return ResponseEntity.status(200).body(updatedPersonDto);
+		} catch(ResourceNotFoundException e) {
+			logger.error(e.getMessage());
+			return null;
+		}
 	}
 	
 	public ResponseEntity<Person> deletePerson(@RequestBody Person person) {
