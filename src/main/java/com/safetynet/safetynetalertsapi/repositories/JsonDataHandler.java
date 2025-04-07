7f0c8ba165bc4d97bcacbb5f96274d8da9769a5f
divineion <najima.guermoudi@gmail.com>
2025-04-07 22:38:10 +0200

    com.safetynet.safetynetalertsapi.repositories.JsonDataHandler.java
    handle FireStation in the update() method.
    
    - Add a condition in the update() method to handle FireStation resources.
    - Search for a matching FireStation by address in the dataset.
    - Replace the existing entry with the new one if a match is found.
    - Update the fireStations list in the dataset.
    - Rewrite the data source file.
    
    Format code

--- src/main/java/com/safetynet/safetynetalertsapi/repositories/JsonDataHandler.java
+++ src/main/java/com/safetynet/safetynetalertsapi/repositories/JsonDataHandler.java
@@ -22,138 +22,144 @@ import com.safetynet.safetynetalertsapi.model.Person;
 @Repository
 public class JsonDataHandler implements DataHandler {
 
-	private final Logger logger = LogManager.getLogger(JsonDataHandler.class);
+    private final Logger logger = LogManager.getLogger(JsonDataHandler.class);
 
-	//			//Java 8 date/time type `java.time.LocalDate` not supported by default
-	//https://www.javacodegeeks.com/jackson-java-8-date-time-localdate-support-issues.html
-	//configure ObjectMapper
-	private static final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule())
-			.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-			.enable(SerializationFeature.INDENT_OUTPUT);
-	
-	@Autowired
-	private DataSetLoader dataSetLoader;
+    //			//Java 8 date/time type `java.time.LocalDate` not supported by default
+    //https://www.javacodegeeks.com/jackson-java-8-date-time-localdate-support-issues.html
+    //configure ObjectMapper
+    private static final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .enable(SerializationFeature.INDENT_OUTPUT);
 
-	@Autowired
-	private StringFormatter formatter;
+    @Autowired
+    private DataSetLoader dataSetLoader;
 
-	private static final File file = new File(DataBaseFilePaths.DATABASE_JSON_PATH);
+    @Autowired
+    private StringFormatter formatter;
 
-	public DataSet getAllData() {
-		return dataSetLoader.getDataSet();
-	}
-	
-	public List<Person> findAllPersons() {
-		return getAllData().getPersons();
-	}
-	
-	public List<FireStation> findAllFireStations() {
-		return getAllData().getFireStations();
-	}
-	
-	public List<MedicalRecord> findAllMedicalRecords() {
-		return getAllData().getMedicalRecords();
-	}
+    private static final File file = new File(DataBaseFilePaths.DATABASE_JSON_PATH);
 
-	public void write(Object resource) {
-		try {
-			//représenter la structure complète du fichier
-			DataSet existingData = mapper.readValue(file, DataSet.class);
-			//ajouter les données
-			if (resource instanceof Person) {
-				existingData.getPersons().add((Person) resource);
-			}
+    public DataSet getAllData() {
+        return dataSetLoader.getDataSet();
+    }
 
-			if (resource instanceof FireStation) {
-				existingData.getFireStations().add((FireStation) resource);
-			}
+    public List<Person> findAllPersons() {
+        return getAllData().getPersons();
+    }
 
-			if (resource instanceof MedicalRecord) {
-				existingData.getMedicalRecords().add((MedicalRecord) resource);
-			}
-			// réécrire tte la structure mise à jour
-			mapper.writeValue(file, existingData);  //
+    public List<FireStation> findAllFireStations() {
+        return getAllData().getFireStations();
+    }
 
-		} catch (
-				IOException e) {
-			logger.error("Failed to write object of type {}: {}", resource.getClass().getSimpleName(), e.getMessage());
-		}
-	}
+    public List<MedicalRecord> findAllMedicalRecords() {
+        return getAllData().getMedicalRecords();
+    }
 
-	public void update(Object resource) {
-		try {
-			DataSet existingData = mapper.readValue(file, DataSet.class);
-			// vérifier le type de resource et si OK affecter les propriétés à une nouvelle instance
-			if (resource instanceof Person newPerson) {
-				//stocker les data en argument
+    public void write(Object resource) {
+        try {
+            //représenter la structure complète du fichier
+            DataSet existingData = mapper.readValue(file, DataSet.class);
+            //ajouter les données
+            if (resource instanceof Person) {
+                existingData.getPersons().add((Person) resource);
+            }
+
+            if (resource instanceof FireStation) {
+                existingData.getFireStations().add((FireStation) resource);
+            }
+
+            if (resource instanceof MedicalRecord) {
+                existingData.getMedicalRecords().add((MedicalRecord) resource);
+            }
+            // réécrire tte la structure mise à jour
+            mapper.writeValue(file, existingData);  //
+
+        } catch (
+                IOException e) {
+            logger.error("Failed to write object of type {}: {}", resource.getClass().getSimpleName(), e.getMessage());
+        }
+    }
+
+    public void update(Object resource) {
+        try {
+            DataSet existingData = mapper.readValue(file, DataSet.class);
+            // vérifier le type de resource et si OK affecter les propriétés à une nouvelle instance
+            if (resource instanceof Person newPerson) {
+                //stocker les data en argument
                 List<Person> persons = existingData.getPersons();
 
-				//chercher la personne dans la liste
+                //chercher la personne dans la liste
                 for (int i = 0; i < persons.size(); i++) {
-					Person existingPerson = persons.get(i);
+                    Person existingPerson = persons.get(i);
 
                     //si la personne est trouvée, on la remplace par les prop de la ressource fournie
-                    if (formatter.normalizeString(newPerson.getIdentity().toString()).equals(formatter.normalizeString(persons.get(i).getIdentity().toString()))){
-						persons.set(i, newPerson);
-						break;
+                    if (formatter.normalizeString(newPerson.getIdentity().toString()).equals(formatter.normalizeString(persons.get(i).getIdentity().toString()))) {
+                        persons.set(i, newPerson);
+                        break;
                     }
                 }
-				existingData.setPersons(persons);
-			}
+                existingData.setPersons(persons);
+            }
 
-			// réécrire tte la structure mise à jour
-			mapper.writeValue(file, existingData);
+            if (resource instanceof FireStation newFireStation) {
+                List<FireStation> fireStations = existingData.getFireStations();
 
-//			if (resource instanceof FireStation) {
-//				existingData.getFireStations().add((FireStation) resource);
-//			}
+                for (int i = 0; i < fireStations.size(); i++) {
+                    FireStation existingFireStation = fireStations.get(i);
 
-//			if (resource instanceof MedicalRecord) {
-//				existingData.getMedicalRecords().add((MedicalRecord) resource);
-//			}
-		} catch (IOException e) {
-			logger.error(e.getMessage());
-			throw new RuntimeException(e);
-		}
-	}
+                    if (newFireStation.getAddress().equals(existingFireStation.getAddress())) {
+                        System.out.println("firestationdto depuis le handler :  " + newFireStation);
 
-	public <T> void delete(Class<T> type, String uniqueIdentifier) {
-		try {
-			DataSet existingData = mapper.readValue(file, DataSet.class);
+                        fireStations.set(i, newFireStation);
+                        break;
+                    }
+                }
+                existingData.setFireStations(fireStations);
+            }
+            mapper.writeValue(file, existingData);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
 
-			if (type.equals(Person.class)) {
-				List<Person> persons = existingData.getPersons();
+    public <T> void delete(Class<T> type, String uniqueIdentifier) {
+        try {
+            DataSet existingData = mapper.readValue(file, DataSet.class);
 
-				//chercher la personne dans la liste
-				for (int i = 0; i < persons.size(); i++) {
-					Person personToDelete = persons.get(i);
+            if (type.equals(Person.class)) {
+                List<Person> persons = existingData.getPersons();
 
-					//si la personne est trouvée, on la supprime
-					if (formatter.normalizeString(uniqueIdentifier).equals(formatter.normalizeString(personToDelete.getIdentity().toString()))) {
-						persons.remove(personToDelete);
-						break;
-					}
-				}
-				existingData.setPersons(persons);
-			}
+                //chercher la personne dans la liste
+                for (int i = 0; i < persons.size(); i++) {
+                    Person personToDelete = persons.get(i);
 
-			if (type.equals(FireStation.class)) {
-				List<FireStation> fireStations = existingData.getFireStations();
+                    //si la personne est trouvée, on la supprime
+                    if (formatter.normalizeString(uniqueIdentifier).equals(formatter.normalizeString(personToDelete.getIdentity().toString()))) {
+                        persons.remove(personToDelete);
+                        break;
+                    }
+                }
+                existingData.setPersons(persons);
+            }
 
-				for (int i = 0; i < fireStations.size(); i++) {
-					FireStation fireStationToDelete = fireStations.get(i);
+            if (type.equals(FireStation.class)) {
+                List<FireStation> fireStations = existingData.getFireStations();
 
-					if (formatter.normalizeString(uniqueIdentifier).equals(formatter.normalizeString(fireStationToDelete.toString()))) {
-						fireStations.remove(fireStationToDelete);
-						break;
-					}
-				}
-				existingData.setFireStations(fireStations);
-			}
+                for (int i = 0; i < fireStations.size(); i++) {
+                    FireStation fireStationToDelete = fireStations.get(i);
 
-			mapper.writeValue(file, existingData);
-		} catch (IOException e) {
-			logger.error(e.getMessage());
+                    if (formatter.normalizeString(uniqueIdentifier).equals(formatter.normalizeString(fireStationToDelete.toString()))) {
+                        fireStations.remove(fireStationToDelete);
+                        break;
+                    }
+                }
+                existingData.setFireStations(fireStations);
+            }
+
+            mapper.writeValue(file, existingData);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
             throw new RuntimeException(e);
         }
     }
