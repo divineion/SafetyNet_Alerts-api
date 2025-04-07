b16d92f1ac336d7586bbcd7efe5467c0dd333e17
divineion <najima.guermoudi@gmail.com>
2025-04-07 22:25:51 +0200

    com.safetynet.safetynetalertsapi.repositories.FireStationRepository.java
    Add update() method that:
    - Checks if the address in the request body matches the URL parameter and throws an InvalidAddressException otherwise.
    - Checks if the provided address as parameter matches a station address from the database, throws a ResourceNotFoundException otherwise.
    - Calls the JsonDataHandler update() method and returns the updated FireStation.
    - Format code, remove debug prints.

--- src/main/java/com/safetynet/safetynetalertsapi/repositories/FireStationRepository.java
+++ src/main/java/com/safetynet/safetynetalertsapi/repositories/FireStationRepository.java
@@ -25,15 +25,12 @@ public class FireStationRepository {
     public FireStation save(FireStation fireStation) throws ResourceAlreadyExistsException, IOException {
         List<FireStation> fireStations = dataHandler.findAllFireStations();
 
-        System.out.println("fire station to string : "+fireStation.toString());
-
         if (fireStations.stream().anyMatch(fs -> {
             boolean match = fs.equals(fireStation);
             System.out.println("Comparing: " + fs + " with " + fireStation + " => " + match);
 
             return match;
-        }))
-        {
+        })) {
             throw new ResourceAlreadyExistsException(fireStation.toString() + " is already in the database");
         }
         dataHandler.write(fireStation);
@@ -46,10 +43,27 @@ public class FireStationRepository {
 
         if (fireStations.stream().noneMatch(fs -> {
             return formatter.normalizeString(fs.toString()).equals(identifier);
-        }))
-        {
+        })) {
             throw new ResourceNotFoundException("The station with the provided address and station number is not found.");
         }
         dataHandler.delete(FireStation.class, identifier);
+    }
+
+    public FireStation update(FireStation fireStation, String address) throws ResourceNotFoundException, InvalidAddressException {
+        if (!formatter.normalizeString(fireStation.getAddress()).equals(formatter.normalizeString(address))) {
+            throw new InvalidAddressException("The provided address in the request does not match the fire station address.");
+        }
+
+        List<FireStation> fireStations = dataHandler.findAllFireStations();
+
+        if (fireStations.stream().noneMatch(fs -> {
+            return (formatter.normalizeString(fs.getAddress()).equals(formatter.normalizeString(address)));
+        })) {
+            throw new ResourceNotFoundException("No station exists for the provided address.");
+        }
+
+        dataHandler.update(fireStation);
+
+        return fireStation;
     }
 }
