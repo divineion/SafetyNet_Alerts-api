9c1cd526d88ed9bdc7474b1e86273d0f538b5855
divineion <najima.guermoudi@gmail.com>
2025-04-07 08:10:12 +0200

    src/main/java/com/safetynet/safetynetalertsapi/exceptions/ResourceAlreadyExistsException.java:
    Rename PersonAlreadyExistsException to ResourceAlreadyExistsException

--- src/main/java/com/safetynet/safetynetalertsapi/exceptions/PersonAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.safetynet.safetynetalertsapi.exceptions;
-
-public class PersonAlreadyExistsException extends Throwable {
-    public PersonAlreadyExistsException(String message) {
-        super(message);
-    }
-}
