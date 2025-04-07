893c74b61c8ecb5db9201ae2415d1b5cbdc00cb9
divineion <najima.guermoudi@gmail.com>
2025-04-07 08:46:04 +0200

    src/main/java/com/safetynet/safetynetalertsapi/persisters/FireStationPersister.java:
    Add method saveFireStation() that:
    - maps the FireStationDTO to domain object,
    - pass it to the repository's save() method,
    - maps the result back to a FireStationDTO,
    - returns the DTO to the controller,
    - catches IOException and wraps it in a RuntimeException,
    - throws an ResourceAlreadyException if a FireStation with the same address and the same station number already exists.

---
+++ src/main/java/com/safetynet/safetynetalertsapi/services/persisters/FireStationPersister.java
@@ -0,0 +1,41 @@
+package com.safetynet.safetynetalertsapi.services.persisters;
+
+import com.safetynet.safetynetalertsapi.exceptions.ResourceAlreadyExistsException;
+import com.safetynet.safetynetalertsapi.model.FireStation;
+import com.safetynet.safetynetalertsapi.model.dto.FireStationDTO;
+import com.safetynet.safetynetalertsapi.repositories.FireStationRepository;
+import com.safetynet.safetynetalertsapi.services.mappers.FireStationMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class FireStationPersister {
+
+    private final Logger logger = LogManager.getLogger(FireStationPersister.class);
+
+    @Autowired
+    FireStationRepository repository;
+
+    @Autowired
+    FireStationMapper mapper;
+
+
+    public FireStationDTO saveFireStation(FireStationDTO fireStationDTO) throws ResourceAlreadyExistsException {
+        FireStation fireStation = mapper.fromFireStationDtoToFireStation(fireStationDTO);
+        try {
+            FireStation savedFireStation = repository.save(fireStation);
+
+            FireStationDTO responseDtoFireStation =  mapper.fromFireStationToFireStationDTO(savedFireStation);
+
+            return responseDtoFireStation;
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+
+            throw new RuntimeException();
+        }
+    }
+}
