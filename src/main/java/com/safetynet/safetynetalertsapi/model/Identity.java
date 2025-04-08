c4fbac4aa2dc9492333678262eaabf0e52e34e0f
divineion <najima.guermoudi@gmail.com>
2025-04-08 14:50:28 +0200

    feat/POST medical record
    src.main.java.com.safetynet.safetynetalertsapi.model.MedicalRecord.java
    
    src.main.java.com.safetynet.safetynetalertsapi.model.Identity.java
    
    src.main.java.com.safetynet.safetynetalertsapi.model.dto.MedicalRecord.java
    
    Create DTO to represent a medical record structure.
    
    Format MedicalRecord.java code

--- src/main/java/com/safetynet/safetynetalertsapi/model/Identity.java
+++ src/main/java/com/safetynet/safetynetalertsapi/model/Identity.java
@@ -5,49 +5,49 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Objects;
 
 public class Identity {
-	private String firstName;
-	
-	private String lastName;
+    private String firstName;
 
-	public Identity() {}
+    private String lastName;
 
-	public Identity(@JsonProperty("firstName") String firstName, @JsonProperty("lastName") String lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
+    public Identity() {
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public Identity(@JsonProperty("firstName") String firstName, @JsonProperty("lastName") String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	@Override
-	public String toString() {
-		return this.firstName + this.lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Identity identity = (Identity) o;
-		return Objects.equals(firstName, ((Identity) o).getFirstName()) &&
-				Objects.equals(lastName, ((Identity) o).getLastName());
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(firstName, lastName);
-	}
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Identity identity = (Identity) o;
+        return Objects.equals(firstName, ((Identity) o).getFirstName()) && Objects.equals(lastName, ((Identity) o).getLastName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
 }
--- src/main/java/com/safetynet/safetynetalertsapi/model/MedicalRecord.java
+++ src/main/java/com/safetynet/safetynetalertsapi/model/MedicalRecord.java
@@ -6,65 +6,73 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import org.springframework.stereotype.Component;
 
 /**
  * Represents the medical record of a {@link Person} person in the SafetyNet Alerts system.
  * This class contains personal medical information such as the person's name, birth date,
- * allergies, and medications. 
+ * allergies, and medications.
  */
 
-public class MedicalRecord implements Identifiable { 
-	@JsonUnwrapped
+@Component
+public class MedicalRecord implements Identifiable {
+    @JsonUnwrapped
     private Identity identity;
-	
-	@JsonProperty(value="birthdate")
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+
+    @JsonProperty(value = "birthdate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
     private LocalDate birthDate;
-    
-	@JsonProperty("allergies")
+
+    @JsonProperty("allergies")
     private List<String> allergies;
-    
-	@JsonProperty("medications")
+
+    @JsonProperty("medications")
     private List<String> medications;
-    
-	public Identity getIdentity() {
-		return identity;
-	}
-	
-	public void setIdentity(Identity identity) {
-		this.identity = identity;
-	}
-	
-	public LocalDate getBirthDate() {
-		return birthDate;
-	}
-	
-	public void setBirthDate(LocalDate birthDate) {
-		this.birthDate = birthDate;
-	}
-	
-	public List<String> getAllergies() {
-		return allergies;
-	}
-	
-	public void setAllergies(List<String> allergies) {
-		this.allergies = allergies;
-	}
-	
-	public List<String> getMedications() {
-		return medications;
-	}
-	
-	public void setMedications(List<String> medications) {
-		this.medications = medications;
-	}
-	
-	@Override
-	public String toString() {
-		return 
-		 this.identity.toString()
-		 + " " + this.birthDate
-		 + " " + this.allergies
-		 + " " + this.medications;
-	}
+
+    public MedicalRecord() {
+    }
+
+    public MedicalRecord(Identity identity, LocalDate birthDate, List<String> allergies, List<String> medications) {
+        this.identity = identity;
+        this.birthDate = birthDate;
+        this.allergies = allergies;
+        this.medications = medications;
+    }
+
+    public Identity getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(Identity identity) {
+        this.identity = identity;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<String> getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(List<String> allergies) {
+        this.allergies = allergies;
+    }
+
+    public List<String> getMedications() {
+        return medications;
+    }
+
+    public void setMedications(List<String> medications) {
+        this.medications = medications;
+    }
+
+    @Override
+    public String toString() {
+        return this.identity.toString() + this.birthDate + this.allergies + this.medications;
+    }
 }
