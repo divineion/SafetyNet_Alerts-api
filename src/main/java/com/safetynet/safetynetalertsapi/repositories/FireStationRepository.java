8e053018b98d3148178ced7aef88c4c8680425fa
divineion <najima.guermoudi@gmail.com>
2025-04-10 16:32:28 +0200

    feat/add test methods
    src.main.java.com.safetynetalerts.safetynetalertsapi.persisters.FireStationPersister.java:
    Update imports.
    
    FireStationRepository.java:
    Update imports, format address String before comparison.
    
    FireStationValidator.java:
    Update imports, format code.

--- src/main/java/com/safetynet/safetynetalertsapi/repositories/FireStationRepository.java
+++ src/main/java/com/safetynet/safetynetalertsapi/repositories/FireStationRepository.java
@@ -9,7 +9,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.io.IOException;
 import java.util.List;
 
 @Repository
@@ -43,7 +42,7 @@ public class FireStationRepository implements BaseRepository<FireStation> {
 
         FireStation fireStationToDelete = fireStations
                 .stream()
-                .filter(fs -> formatter.normalizeString(fs.toString()).equals(identifier))
+                .filter(fs -> formatter.normalizeString(fs.toString()).equals(formatter.normalizeString(identifier)))
                 .findFirst()
                 .orElseThrow(() -> new ResourceNotFoundException("The station with the provided address and station number is not found."));
 
