717ed5aeea4f622e406ce978080d43dd7e1a3b1c
divineion <najima.guermoudi@gmail.com>
2025-04-15 17:14:21 +0200

    clean code: rename properties
    FireStationCoverageDTO.java :
    rename "childrenCounter" and "adultsCounter" to "children" and "adults" properties.
    Remove unnecessary annotations Json>Creator and JsonProperty.

--- src/main/java/com/safetynet/safetynetalertsapi/model/dto/FireStationCoverageDTO.java
+++ src/main/java/com/safetynet/safetynetalertsapi/model/dto/FireStationCoverageDTO.java
@@ -2,35 +2,29 @@ package com.safetynet.safetynetalertsapi.model.dto;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class FireStationCoverageDTO {
-	@JsonProperty("covered persons")
-	private List<CoveredPersonDTO> coveredPersonsDTO;
-	
-	@JsonProperty("adults")
-	long adultsCounter;
-	
-	@JsonProperty("children")
-	long childrenCounter;
+    @JsonProperty("covered persons")
+    private List<CoveredPersonDTO> coveredPersonsDTO;
+    long adults;
+    long children;
 
-	@JsonCreator
-	public FireStationCoverageDTO(List<CoveredPersonDTO> coveredPersonsDTO, long adultsCounter, long children) {
-		this.coveredPersonsDTO = coveredPersonsDTO;
-		this.adultsCounter = adultsCounter;
-		this.childrenCounter = children;
-	}
+    public FireStationCoverageDTO(List<CoveredPersonDTO> coveredPersonsDTO, long adults, long children) {
+        this.coveredPersonsDTO = coveredPersonsDTO;
+        this.adults = adults;
+        this.children = children;
+    }
 
-	public List<CoveredPersonDTO> getCoveredPersonsDTO() {
-		return coveredPersonsDTO;
-	}
+    public List<CoveredPersonDTO> getCoveredPersonsDTO() {
+        return coveredPersonsDTO;
+    }
 
-	public long getAdultsCounter() {
-		return adultsCounter;
-	}
+    public long getAdults() {
+        return adults;
+    }
 
-	public long getChildrenCounter() {
-		return childrenCounter;
-	}
+    public long getChildren() {
+        return children;
+    }
 }
